0f7b8f941409f6c0bd025adcad88f818c4c1e484
Hoku Tobin <htobin@hawaii.edu>
2022-03-02 17:12:17 -1000

    Script to interact with a simple contract added

--- compose.py
+++ compose.py
@@ -1,31 +1,35 @@
-#File: compose.py
-#Description: Contains functions that create the Docker compose file
+# File: compose.py
+# Description: Contains functions that create the Docker compose file
 import argparse
 import ipaddress
 
 from yaml import parse
 
 
-#Function: ingest_config_file
-#Description: Argument error checking and handler
-#Return: the argument being passed in
+# Function: ingest_config_file
+# Description: Argument error checking and handler
+# Return: the argument being passed in
 
 def ingest_config_file():
-    parser = argparse.ArgumentParser(description='create docker compose file, directories for nodes, and Dockerfiles for nodes')
-    parser.add_argument('config_file', help='keyfile that contains information about bootnode, amount of nodes, IP range for nodes, and the network ID')
+    parser = argparse.ArgumentParser(
+        description='create docker compose file, directories for nodes, and Dockerfiles for nodes')
+    parser.add_argument('config_file',
+                        help='keyfile that contains information about bootnode, amount of nodes, IP range for nodes, '
+                             'and the network ID')
     file = parser.parse_args()
     return file
 
-#Function: create_bootnode_data
-#Description: Takes in the boot node data, and puts it into a dictionary that's easy to reference
-#Arguments:
+
+# Function: create_bootnode_data
+# Description: Takes in the boot node data, and puts it into a dictionary that's easy to reference
+# Arguments:
 #          key: hex key of the boot node
 #          enode: the enode address so that way other nodes can connect to the boot node
 #          IP: IP address of the boot node
 #          port: discover port of the boot node   
-#Return: The argument being passed in
+# Return: The argument being passed in
 
-def create_bootnode_data(key,enode,IP,port):
+def create_bootnode_data(key, enode, IP, port):
     data = {
         "nodehexkey": key,
         "enode": enode,
@@ -34,112 +38,112 @@ def create_bootnode_data(key,enode,IP,port):
     }
     return data
 
-#Function: create_bootnode
-#Description: Parses boot node data and creates the boot node Docker compose section
-#Arguments:
+
+# Function: create_bootnode
+# Description: Parses boot node data and creates the boot node Docker compose section
+# Arguments:
 #          bnode_data: dictionary with information of the boot node
-#Return: bootnode: boot node portion of the Docker compose file
+# Return: bootnode: boot node portion of the Docker compose file
 
 def create_bootnode(bnode_data):
-    #nodehexkey string creation
+    # nodehexkey string creation
     nodekeyhex_string = "nodekeyhex=" + bnode_data["nodehexkey"]
-    bnode_port_string = bnode_data["port"]+":"+ bnode_data["port"] + "/udp"
-    bnode_port_environment = "port="+bnode_data["port"]
+    bnode_port_string = bnode_data["port"] + ":" + bnode_data["port"] + "/udp"
+    bnode_port_environment = "port=" + bnode_data["port"]
     bootnode = {
         'hostname': 'geth-bootnode',
         'environment': [
-                nodekeyhex_string,
-                bnode_port_environment
-            ],
-            'build': {
-                'context' :'./geth-bootnode'
-            },
-            'ports': [
-                bnode_port_string
-            ],
-            'volumes':["bootnode:/root/.ethash"],
-            'networks':{
-                'chainnet':{
-                    'ipv4_address':bnode_data["ip"]
-                }
-            }   
+            nodekeyhex_string,
+            bnode_port_environment
+        ],
+        'build': {
+            'context': './geth-bootnode'
+        },
+        'ports': [
+            bnode_port_string
+        ],
+        'volumes': ["bootnode:/root/.ethash"],
+        'networks': {
+            'chainnet': {
+                'ipv4_address': bnode_data["ip"]
+            }
+        }
     }
 
     return bootnode
 
 
-#Function: create_nodes_wrapper
-#Description: Creates the node dictionary keys and values
-#Arguments:
+# Function: create_nodes_wrapper
+# Description: Creates the node dictionary keys and values
+# Arguments:
 #          net_range: range of the IP addresses that will be used for all nodes
 #          n_count: number of nodes that will be created
 #          bnode_data: dictionary with information of the boot node
 #          chain_id: ID of the blockchain where work will be commited
-#Return: node_obj: Dictionary of all nodes and the information about each individual node
+# Return: node_obj: Dictionary of all nodes and the information about each individual node
 
 def create_nodes_wrapper(net_range, n_count, bnode_data, chain_id):
-    #create the base IP address for the first node that's not a boot node
+    # create the base IP address for the first node that's not a boot node
     end_of_ip_range = net_range.find("/")
     ip = net_range[:end_of_ip_range]
     ip_addr = ipaddress.ip_address(ip) + 2
 
-    #counter for incrementing through the amount of nodes
-    i=0
+    # counter for incrementing through the amount of nodes
+    i = 0
 
-    #ports for the first node that's not a boot node
+    # ports for the first node that's not a boot node
     rpc_port = 8545
     discover_port = 30302
 
-    #total nodes
+    # total nodes
     n_count = int(n_count)
     node_obj = {}
 
-
     while i < n_count:
-        node = create_node(str(i),str(ip_addr), bnode_data, str(rpc_port), str(discover_port), str(chain_id))
+        node = create_node(str(i), str(ip_addr), bnode_data, str(rpc_port), str(discover_port), str(chain_id))
         node_obj[node["hostname"]] = node
 
-        #increment ports, IP address, and the node name by 1
-        rpc_port +=1
-        discover_port +=1
-        ip_addr +=1
-        i+=1
+        # increment ports, IP address, and the node name by 1
+        rpc_port += 1
+        discover_port += 1
+        ip_addr += 1
+        i += 1
     return node_obj
 
-#Function: create_node
-#Description: Creates the current node's Docker compose portion
-#Arguments:
+
+# Function: create_node
+# Description: Creates the current node's Docker compose portion
+# Arguments:
 #          net_range: range of the IP addresses that will be used for all nodes
 #          n_count: number of nodes that will be created
 #          bnode_data: dictionary with information of the boot node
 #          chain_id: ID of the blockchain where work will be commited
-#Return: node: Docker compose portion for the node 
+# Return: node: Docker compose portion for the node
 
-def create_node(num,node_ip, bnode_data, rpc, discover, net_id):
-    node_name = "node_"+ num
-    n_name= "geth-dev-" + node_name
-    
-    #Docker environment variables for the node
+def create_node(num, node_ip, bnode_data, rpc, discover, net_id):
+    node_name = "node_" + num
+    n_name = "geth-dev-" + node_name
+
+    # Docker environment variables for the node
     rpc_env_string = "rpcPort=" + rpc
-    discover_env_string ="discoverPort=" + discover
+    discover_env_string = "discoverPort=" + discover
     bnode_id_string = "bootnodeId=" + bnode_data["enode"]
     bnode_ip_string = "bootnodeIp=" + bnode_data["ip"]
     bnode_port_string = "bootnodePort=" + bnode_data["port"]
     network_id_string = "networkId=" + net_id
 
-    #ports
-    rpc_port_string = rpc+":"+ rpc
-    discover_port_string = discover+ ":" + discover +"/udp"
+    # ports
+    rpc_port_string = rpc + ":" + rpc
+    discover_port_string = discover + ":" + discover + "/udp"
 
-
-    dockerfile_string = "./"+ n_name +"/Dockerfile"
+    dockerfile_string = "./" + n_name + "/Dockerfile"
     test_string = "wget http://localhost:" + rpc
-    volume_string = "eth-data-"+num+":/root/.ethhash"
+    volume_string = "eth-data-" + num + ":/root/.ethhash"
 
     node = {
         'hostname': n_name,
-        'depends_on':['geth-bootnode'],
-        'environment':[
+        'depends_on': ['geth-bootnode'],
+        'environment': [
             bnode_id_string,
             bnode_ip_string,
             rpc_env_string,
@@ -147,54 +151,56 @@ def create_node(num,node_ip, bnode_data, rpc, discover, net_id):
             bnode_port_string,
             network_id_string
         ],
-        'build':{
+        'build': {
             'context': ".",
             'dockerfile': dockerfile_string
         },
         'container_name': n_name,
-        'healthcheck':{
+        'healthcheck': {
             'test': test_string,
-            'interval':'2s',
-            'timeout':'5s',
-            'retries':'30'
+            'interval': '2s',
+            'timeout': '5s',
+            'retries': '30'
         },
-        'volumes':[
+        'volumes': [
             volume_string
         ],
-        'ports':[
+        'ports': [
             rpc_port_string,
             discover_port_string
         ],
-        'networks':{
-            'chainnet':{
-                'ipv4_address':node_ip
+        'networks': {
+            'chainnet': {
+                'ipv4_address': node_ip
             }
         }
     }
     return node
 
-#Function: create_network
-#Description: Creates the network portion of the Docker compose file
-#Arguments:
+
+# Function: create_network
+# Description: Creates the network portion of the Docker compose file
+# Arguments:
 #          net_range: range of the IP addresses that will be used for all nodes
-#Return: network: network portion of the Docker compose file
+# Return: network: network portion of the Docker compose file
 
 def create_network(net_range):
     network = {
-        'chainnet':{
+        'chainnet': {
             'driver': 'bridge',
-            'ipam':{
-                'config':[{'subnet':net_range}]
+            'ipam': {
+                'config': [{'subnet': net_range}]
             }
         }
-    }   
+    }
     return network
 
-#Function: create_volumes
-#Description: Creates the network portion of the Docker compose file
-#Arguments:
+
+# Function: create_volumes
+# Description: Creates the network portion of the Docker compose file
+# Arguments:
 #          num_nodes: amount of wallet nodes that will be created
-#Return: volumes: volume portion of the Docker compose file
+# Return: volumes: volume portion of the Docker compose file
 
 def create_volumes(num_nodes):
     volumes = {}
@@ -203,5 +209,5 @@ def create_volumes(num_nodes):
     while i < int(num_nodes):
         volume_string = "eth-data-" + str(i)
         volumes[volume_string] = None
-        i+=1
+        i += 1
     return volumes
--- nodes.py
+++ nodes.py
@@ -1,14 +1,51 @@
 import argparse
 import json
 from web3 import Web3
-import sys
+import solcx
+
+
+def contact_interaction(nodes):
+    w3 = nodes['8545']
+    interaction = nodes['8546']
+
+    compiled_sol = solcx.compile_files(['./Greeter.sol'], output_values=['abi', 'bin'])
+    contract_id, contract_interface = compiled_sol.popitem()
+    bytecode = contract_interface['bin']
+    abi = contract_interface['abi']
+
+    # set pre-funded account as sender
+    w3.eth.default_account = w3.eth.accounts[0]
+
+    #create contract instance
+    Greeter = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+    # Submit the transaction that deploys the contract
+    tx_hash = Greeter.constructor().transact()
+
+    # Wait for the transaction to be mined, and get the transaction receipt
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    greeter = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+    print(f"Greeter 1: {greeter.functions.greet().call()}")
+
+    interaction.eth.default_account = interaction.eth.accounts[0]
+
+    greeter2 = interaction.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+    print(f"Greeter 2: {greeter2.functions.greet().call()}")
+
 
 def arguments():
-    parser = argparse.ArgumentParser(description='create docker compose file, directories for nodes, and Dockerfiles for nodes')
-    parser.add_argument('config_file', help='keyfile that contains information about bootnode, amount of nodes, IP range for nodes, and the network ID')
-    parser.add_argument('function',nargs='?',help='function to indicate which')
-    arguments = parser.parse_args()
-    return arguments
+    parser = argparse.ArgumentParser(
+        description='create docker compose file, directories for nodes, and Dockerfiles for nodes')
+    parser.add_argument('config_file',
+                        help='keyfile that contains information about bootnode, amount of nodes, IP range for nodes, '
+                             'and the network ID')
+    parser.add_argument('function', help='function for nodes to perform')
+    ret_args = parser.parse_args()
+    return ret_args
+
 
 def read_config_file(config_file):
     try:
@@ -19,20 +56,22 @@ def read_config_file(config_file):
         exit()
     return config_data
 
+
 def create_node_names(config_node_count):
     node_names = []
     i = 0
     first_port = 8545
     while i < int(config_node_count):
         node_names.append(str(first_port + i))
-        i+=1
+        i += 1
     return node_names
 
+
 def create_node_obj(node_names):
-    node_objs ={}
+    node_objs = {}
     for name in node_names:
         current_string = "http://localhost:" + name
-        node_objs[name] =  Web3(Web3.HTTPProvider(current_string))
+        node_objs[name] = Web3(Web3.HTTPProvider(current_string))
     return node_objs
 
 
@@ -42,6 +81,7 @@ def connect_to_nodes(args):
     node_objs = create_node_obj(node_names)
     return node_objs
 
+
 def connection_check(nodes):
     for name in nodes.keys():
         if nodes[name].isConnected():
@@ -49,63 +89,52 @@ def connection_check(nodes):
         else:
             print(f"{name} is not connected")
 
-def create_accounts(nodes):
-    for name in nodes.keys():
-        nodes[name].geth.personal.newAccount("pass")
 
 def account_check(nodes):
     for name in nodes.keys():
         print(f"{name}:  account: {nodes[name].eth.accounts[0]}")
         print(f"{name}: coinbase: {nodes[name].eth.coinbase}")
 
+
 def start_mining(nodes):
     i = 1
-    while i < len(nodes):
-        name = str(8545+i)
+    while i < len(nodes) - 1:
+        name = str(8546 + i)
         print(f"{name}: starting to mine")
         nodes[name].geth.miner.start(2)
-        if(nodes[name].eth.mining):
+        if nodes[name].eth.mining:
             print(f"{name} is mining")
         else:
             print(f"{name} is not mining")
-        i+=1
+        i += 1
+
 
 def check_transactions(nodes):
     node = input("Please enter node port number: ")
     transaction = input("Please enter transaction number: ")
     print(f"connecting to port: {node}, looking for {transaction}")
-    receipt =nodes[str(node)].eth.get_transaction_receipt(str(transaction)) 
-    if receipt ==  False:
+    receipt = nodes[str(node)].eth.get_transaction_receipt(str(transaction))
+    if receipt == False:
         print("could not find transaction")
-    else: 
-         raw = Web3.toJSON(receipt)
-         parsed = json.loads(raw)
-         print(json.dumps(parsed, indent=2))
-
+    else:
+        raw = Web3.toJSON(receipt)
+        parsed = json.loads(raw)
+        print(json.dumps(parsed, indent=2))
 
 
 def stop_mining(nodes):
     for name in nodes.keys():
         print(f"{name}: mining will stop")
         nodes[name].geth.miner.stop()
-        if(nodes[name].eth.mining):
+        if (nodes[name].eth.mining):
             print(f"{name} is still mining")
         else:
             print(f"{name} is not mining")
 
-def unlock_accounts(nodes):
-    for name in nodes.keys():
-        self= nodes[name]
-        account_to_unlock = self.geth.personal.list_accounts()[0]
-        status = self.geth.personal.unlock_account(account_to_unlock, "pass")
-        if status:
-            print(f"{name} is unlocked")
-        else:
-            print(f"{name} is not unlocked")
 
 def list_balance(nodes):
     for name in nodes.keys():
-        self= nodes[name]
+        self = nodes[name]
         account = self.geth.personal.list_accounts()[0]
         status = self.eth.get_balance(account)
         if status:
@@ -113,34 +142,28 @@ def list_balance(nodes):
         else:
             print(f"could not get balance of {name}")
 
+
 def still_mining(nodes):
     for name in nodes.keys():
-        if(nodes[name].eth.mining):
+        if (nodes[name].eth.mining):
             print(f"{name} is still mining")
         else:
             print(f"{name} is not mining")
 
+
 if __name__ == "__main__":
     args = arguments()
     nodes = connect_to_nodes(args)
-    #connection_check(nodes)
-    if args.function =="create_accounts":
-        create_accounts(nodes)
-        account_check(nodes)
-    if args.function =="start_mine":
+    # connection_check(nodes)
+    if args.function == "start_mine":
         start_mining(nodes)
-    if args.function =="stop_mine":
+    if args.function == "stop_mine":
         stop_mining(nodes)
-    if args.function =="unlock":
-        unlock_accounts(nodes)
-    if args.function =="mine_check":
+    if args.function == "mine_check":
         still_mining(nodes)
-    if args.function =="balance":
+    if args.function == "balance":
         list_balance(nodes)
-    if args.function =="transaction":
+    if args.function == "transaction":
         check_transactions(nodes)
-
-    
-
-
-
+    if args.function == "interact":
+        contact_interaction(nodes)
